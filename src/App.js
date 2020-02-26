d8b209a67145671875e04b38c7dc77e50838cd07
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-02-26 01:05:14 -0500

    add buttons to increment and reset quarter and down

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
+// import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [currentDown, setCurrentDown] = useState(1);
+  const [currentQuarter, setCurrentQuarter] = useState(1);
   const [lionsScore, setLionScore] = useState(0);
   const [tigersScore, setTigersScore] = useState(0);
-  
-  const homeTouchDown = (e) => {
+  const homeTouchDown = e => {
     setLionScore(lionsScore + 7);
-  }
-  const homeFieldGoal = (e) => {
+  };
+  const homeFieldGoal = e => {
     setLionScore(lionsScore + 3);
-  }
-  const awayTouchDown = (e) => {
-    setTigersScore(tigersScore + 7)};
-  const awayFieldGoal = (e) => {
+  };
+  const awayTouchDown = e => {
+    setTigersScore(tigersScore + 7);
+  };
+  const awayFieldGoal = e => {
     setTigersScore(tigersScore + 3);
-  }
-    return (
+  };
+
+  return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
@@ -36,20 +39,82 @@ function App() {
             <div className="away__score">{tigersScore}</div>
           </div>
         </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={homeTouchDown}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={homeFieldGoal}>Home Field Goal</button>
+        <div className="bottomRow">
+          <div className="down">
+            <h3 className="down__title">Down</h3>
+            <div className="down__value">{currentDown}</div>
+          </div>
+          <div className="toGo">
+            <h3 className="toGo__title">To Go</h3>
+            <div className="toGo__value">0</div>
+          </div>
+          <div className="ballOn">
+            <h3 className="ballOn__title">Ball on</h3>
+            <div className="ballOn__value">21</div>
+          </div>
+          <div className="quarter">
+            <h3 className="quarter__title">Quarter</h3>
+            <div className="quarter__value">{currentQuarter}</div>
+          </div>
         </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={awayTouchDown}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={awayFieldGoal}>Away Field Goal</button>
-        </div>
-      </section>
-    </div>
+        <section className="buttons bottomRow">
+          <div className="down_buttons">
+            <button
+              className="down_button"
+              onClick={e => {
+                setCurrentDown(currentDown + 1);
+              }}
+            >
+              Set Down
+            </button>
+            <button
+              className="down_reset_button"
+              onClick={e => {
+                setCurrentDown(1);
+              }}
+            >
+              Reset Down
+            </button>
+          </div>
+          <div className="quarter_buttons">
+            <button
+              className="quarter_button"
+              onClick={e => {
+                setCurrentQuarter(currentQuarter + 1);
+              }}
+            >
+              Set Quarter
+            </button>
+            <button
+              className="quarter_reset_button"
+              onClick={e => {
+                setCurrentQuarter(1);
+              }}
+            >
+              Reset Quarter{" "}
+            </button>
+          </div>
+          <div className="homeButtons">
+            {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+            <button className="homeButtons__touchdown" onClick={homeTouchDown}>
+              Home Touchdown
+            </button>
+            <button className="homeButtons__fieldGoal" onClick={homeFieldGoal}>
+              Home Field Goal
+            </button>
+          </div>
+          <div className="awayButtons">
+            <button className="awayButtons__touchdown" onClick={awayTouchDown}>
+              Away Touchdown
+            </button>
+            <button className="awayButtons__fieldGoal" onClick={awayFieldGoal}>
+              Away Field Goal
+            </button>
+          </div>
+        </section>
+        </section>
+
+      </div>
   );
 }
 
